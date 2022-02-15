a95cc1174ef4bf3ed1d459e3c0248ad8564f4ab3
jihyun park <girl9505@naver.com>
2022-02-15 13:59:44 +0900

    feat: article 도메인 기능

---
+++ src/main/java/com/fastcampus/boardpractice/domain/Article.java
@@ -0,0 +1,52 @@
+package com.fastcampus.boardpractice.domain;
+
+
+import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+public class Article {
+
+    private Long id;
+
+    @Setter
+    private String title;
+    @Setter
+    private String content;
+    @Setter
+    private Integer viewCount;
+    @Setter
+    private Integer likeCount;
+    @Setter
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    @Setter
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    @Setter
+    private String modifiedBy;
+
+    public Article(String title, String content, Integer viewCount, Integer likeCount,
+        String hashtag, String createBy, String modifiedBy) {
+        this.title = title;
+        this.content = content;
+        this.viewCount = viewCount;
+        this.likeCount = likeCount;
+        this.hashtag = hashtag;
+        this.createdBy = createBy;
+        this.modifiedBy = modifiedBy;
+    }
+    void test() {
+        Article article = new Article(
+            "title",
+            "content",
+            0,
+            0,
+            "#java",
+            "jihyun",
+            "jihyun"
+        );
+    }
+}
